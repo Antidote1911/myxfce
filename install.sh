f861281024640faa167f43f0b1b9436be1941965
Antidote1911 <antidote1911@gmail.com>
2025-12-07 10:32:57 +0100

    up

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
 username="$(logname)"
 
 # Check for sudo
@@ -7,6 +11,9 @@ if [ "$EUID" -ne 0 ]; then
   echo "This script must be run with sudo."
   exit 1
 fi
+
+echo "${green}Install full desktop or minimal ?${reset}"
+read -p "1 - Full, 0 - Minimal: " vm_setting
 
 # Deploy system configs
 echo "Deploying system configs..."
@@ -17,10 +24,38 @@ sed -i -e 's|color_scheme=nordic.conf|color_scheme=dark-colors.conf|g' /root/.co
 
 rm /etc/sudoers.d/10-installer
 
+
 # Install the custom package list
 echo "Installing needed packages..."
 pacman -Syy
 pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout $(<packages-repository.txt)
+
+########## Only for full install ###########################
+if [[ $vm_setting == 1 ]]; then
+  echo "${green}Enter password to decrypt personal archive:${reset}"
+  read -p "Password: " password
+  echo "${green}Install full desktop and applications${reset}"
+  pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout $(<extra_pkgs.txt)
+  sudo -u "${username}" rustup toolchain install stable
+  
+  ## Add syno nfs share to autofs
+  mkdir /mnt/Partage /mnt/Photos
+  
+  tee -a /etc/fstab << EOL
+  
+## Synology DS918
+192.168.1.96:/volume1/Partage /mnt/Partage nfs _netdev,noauto,x-systemd.automount,x-systemd.mount-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0
+192.168.1.96:/volume1/Photos  /mnt/Photos  nfs _netdev,noauto,x-systemd.automount,x-systemd.mount-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0
+EOL
+
+  modprobe vboxdrv
+  cryptyrust_cli -d myEncryptedFile -p ${password} -o tmp.tar.gz
+  tar -xf tmp.tar.gz -C /home/${username}/
+  sudo -u "${username}" eval `ssh-agent -s`
+  sudo -u "${username}" /home/${username}/./gitconfig.sh
+  yay -S --noconfirm --answerdiff None --answerclean None filebot rustrover rustrover-jre
+fi
+####################################################
 
 # Deploy user configs
 echo "Deploying user configs..."
